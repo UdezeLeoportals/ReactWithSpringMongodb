cc6b8fae4607b9687b46c4afa3dd96a33ed4141a
Udeze Leonard <49731616+UdezeLeoportals@users.noreply.github.com>
2025-07-12 13:21:11 +0200

    Update axiosConfig.js

--- src/api/axiosConfig.js
+++ src/api/axiosConfig.js
@@ -3,8 +3,8 @@ import axios from 'axios';
 export default axios.create({
     baseURL:'https://9c96-103-106-239-104.ap.ngrok.io',
     headers: {"ngrok-skip-browser-warning": "true"},
-    axios.get(`https://cors-anywhere.herokuapp.com/[YOUR_API_URL]`,{headers: {'Access-Control-Allow-Origin': ''https://9c96-103-106-239-104.ap.ngrok.io'}})
-      .then(response => console.log(response:data);
+    headers: {'Access-Control-Allow-Origin': 'https://9c96-103-106-239-104.ap.ngrok.io'}
+    
   }
    // baseURL: 'http://localhost:8080'
 });
